9dd60bf204c59c2fc154ea548865b8ee01f35c0d
prakash2004k <prakashk22cse@srishakthi.ac.in>
2024-11-19 19:08:34 +0530

    footercommits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,9 +8,9 @@ const Footer = () => {
         <div className="footer-section">
           <h3>Follow Us</h3>
           <div className="social-links">
-            <a href="tel:+91770866409" className="icon fas fa-phone-alt" aria-label="Phone"></a>
+            <a href="tel:+917708666409" className="icon fas fa-phone-alt" aria-label="Phone"></a>
             <a href="mailto:saterlonengineering@gmail.com" className="icon fas fa-envelope" aria-label="Email"></a>
-            <a href="https://www.linkedin.com/in/yourprofile" className="icon fab fa-linkedin" aria-label="LinkedIn"></a>
+            <a href="https://www.linkedin.com/in/prakash-k-a313ab290/" className="icon fab fa-linkedin" aria-label="LinkedIn"></a>
             <a href="https://www.yourwebsite.com" className="icon fas fa-globe" aria-label="Website"></a>
           </div>
         </div>
